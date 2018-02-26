d51adce82c7d97e99a0af871e019fbf49cd7b25e
stanley <stanley.ho@yh.nackademin.se>
2018-02-26 23:53:21 +0100

    Weather status layout

--- assets/scripts/weather.js
+++ assets/scripts/weather.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const KEY = '25fcb5ba7d564b1fc9bab65a004efa5e';
-const API_URL = 'http://api.openweathermap.org/data/2.5/forecast?q=nynashamn&appid=' + KEY;
+const API_URL = 'http://api.openweathermap.org/data/2.5/forecast?q=nynashamn&units=metric&lang=se&appid=' + KEY;
 
 function HttpGet(url) {
   this.url = url;
@@ -22,41 +22,25 @@ function fetch(url) {
   return new HttpGet(url);
 }
 
-function $(selector) {
-  return document.querySelector(selector);
-}
-
-/* function find(selector) {
-  return new DOMElement(selector);
-} */
-
 var weatherObjects = {
   container: '.weather-data',
   objects: []
 };
 
-
 fetch(API_URL).proceed(response => {
-  var weatherData = JSON.parse(response);
-  var weatherList = weatherData.list;
-  
+  let weatherData = JSON.parse(response);
+  let weatherList = weatherData.list;
+
   for (let i = 0; i < 5; i++) {
-    var hours = new Date(weatherList[i].dt_txt).getHours() + ":00";
-    var windSpeed = weatherList[i].wind.speed;
-    var temperature = weatherList[i].main.temp;
-    var weather = weatherList[i].weather[0].description;
-
-    var weather1 = new Weather(hours, weather, temperature, windSpeed);
-    console.log(weatherObjects.objects);
-    weatherObjects.objects.push(weather1);
-    /* weatherObjects.objects.push(new Weather(hours, weather, temperature, windSpeed)); */
-
-    console.log(hours);
-    console.log(windSpeed);
-    console.log(temperature);
-    console.log(weather);
+    let hours = new Date(weatherList[i].dt_txt).getHours() + ":00";
+    let windSpeed = Number(weatherList[i].wind.speed).toFixed(1) + "m/s";
+    let temperature = Number(weatherList[i].main.temp).toFixed(1) + "&deg;C";
+    let weatherState = weatherList[i].weather[0].description;
+    
+    let weatherObject = new Weather(hours, weatherState, temperature, windSpeed);
+    weatherObjects.objects.push(weatherObject);
   }
+  var weather = new WeatherTable(weatherObjects);
+  weather.putData();
 });
 
-var weatherTest = new WeatherTable(weatherObjects);
-weatherTest.putData();
